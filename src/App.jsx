c11822c7bfa6409507a7ff236f27ad201baee81f
Adrian Abella <fasttaco1@gmail.com>
2024-10-15 21:40:08 -0400

    Simple elements and styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
     const [count, setCount] = useState(0);
+    const [film, setFilm] = useState({});
 
-    return <></>;
-}
+    const handleFind = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <div className="page">
+            <div className="history-list">
+                <h3>HISTORY</h3>
+            </div>
+            <div className="film-listing-container">
+                <h2>TRIPLE A FILM RECOMMENDATION SYSTEM</h2>
+                {count != 0 && <div>MOVIE</div>}
+                <button onClick={handleFind}>FIND FILM 🔀</button>
+            </div>
+            <div className="ban-list">
+                <h3>BAN LIST</h3>
+                <p>
+                    Select an attribute in the film listing to never see it
+                    again.
+                </p>
+            </div>
+        </div>
+    );
+};
 
 export default App;

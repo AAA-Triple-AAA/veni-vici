5755d1c01bd289f441cd362df8c082fbc14c3793
Adrian Abella <fasttaco1@gmail.com>
2024-10-15 23:40:51 -0400

    Added successful API calls to TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
 import { useState } from "react";
 import "./App.css";
 
+const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
+
 const App = () => {
     const [count, setCount] = useState(0);
     const [film, setFilm] = useState({});
 
-    const handleFind = () => {
+    const handleFind = async () => {
+        const film = await getRandomMovie();
+        setFilm(film);
+        console.log(film);
         setCount(count + 1);
     };
+
+    async function getLatestMovieId() {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/movie/latest?api_key=${ACCESS_KEY}`
+        );
+        const data = await response.json();
+        return data.id;
+    }
+
+    async function getRandomMovieId() {
+        const latestId = await getLatestMovieId();
+        return Math.floor(Math.random() * latestId) + 1;
+    }
+
+    async function getRandomMovie() {
+        let movieData;
+        do {
+            const randomId = await getRandomMovieId();
+            const response = await fetch(
+                `https://api.themoviedb.org/3/movie/${randomId}?api_key=${ACCESS_KEY}&include_adult=false&vote_count.gte=50&popularity.gte=10`
+            );
+            movieData = await response.json();
+        } while (
+            movieData.status_code === 34 ||
+            !movieData.poster_path ||
+            movieData.genres.length == 0
+        ); // Retry if movie not found
+        return movieData;
+    }
 
     return (
         <div className="page">
@@ -16,7 +50,15 @@ const App = () => {
             </div>
             <div className="film-listing-container">
                 <h2>TRIPLE A FILM RECOMMENDATION SYSTEM</h2>
-                {count != 0 && <div>MOVIE</div>}
+                {count != 0 && (
+                    <div>
+                        <h1>{film.title}</h1>
+                        <img
+                            src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+                            alt="poster"
+                        />
+                    </div>
+                )}
                 <button onClick={handleFind}>FIND FILM 🔀</button>
             </div>
             <div className="ban-list">
